7987fdeae12a129abae8928cbbae68aa25290366
Auwal MS <auwalforall@gmail.com>
2018-09-02 14:29:31 +0100

    fix: added `ignoreSearch: true` to ensure the cache matches the page irrespective of its search query.

--- service-worker.js
+++ service-worker.js
@@ -55,7 +55,7 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
             if (response) {
                 return response;
             }
